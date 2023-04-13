53129b8b835a14e012aca86ee0709624a5d06f6e
Ayomide S.E.P <ayomidesunday31@gmail.com>
2023-04-13 14:45:13 +0100

    Anagram problem soloved by :- driver Ayomide Sunday Emmanuel & navigator :- Md. Porag Sarkar

--- anagram-checker.js
+++ anagram-checker.js
@@ -1,3 +1,14 @@
 module.exports = function (string1, string2) {
-  // write your code here
+
+  if(string1.length !== string2.length){
+    return false;
+  }
+  let str1 = string1.toLowerCase().split('').sort().join('');
+  let str2 = string2.toLowerCase().split('').sort().join('');
+
+  if(str1 === str2){
+    return true;
+  } else { 
+    return false;
+  }
 }
