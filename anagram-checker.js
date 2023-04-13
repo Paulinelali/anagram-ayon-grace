561fa1fa47806ea92d6138482ef1d2084a9eaf58
Ayomide S.E.P <ayomidesunday31@gmail.com>
2023-04-13 16:02:36 +0100

    Anagram solved by :- driver Ayomide Sunday Emmanuel & navigator Md. Porag Sarkar

--- anagram-checker.js
+++ anagram-checker.js
@@ -1,11 +1,19 @@
 module.exports = function (string1, string2) {
+  let noSpace1 = string1.replace(/\s+/g, '');
+  let noSpace2 = string2.replace(/\s+/g, '');
+  let noSpecialChar1 = noSpace1.replace(/[^a-zA-Z ]/g, "");
+  let noSpecialChar2 = noSpace2.replace(/[^a-zA-Z ]/g, "")
 
-  if(string1.length !== string2.length){
-    return false;
+  if(noSpecialChar1.length !== noSpecialChar2.length){
+    console.log("Invalid")
+    return 
   }
-  let str1 = string1.toLowerCase().split('').sort().join('');
-  let str2 = string2.toLowerCase().split('').sort().join('');
 
+  let str1 = noSpecialChar1.toLowerCase().split('').sort().join('');
+  let str2 = noSpecialChar2.toLowerCase().split('').sort().join('');
+  console.log(str1)
+  console.log(str2)
+  
   if(str1 === str2){
     return true;
   } else { 
